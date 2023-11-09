0db5610697050eb079b82015ef3b0e75e3f76e74
rbaruah <31023635+Rakesh6720@users.noreply.github.com>
2023-11-09 16:57:49 +0000

    Update CourseGoalList.tsx

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import { type CourseGoal as CourseGoalType } from "../types/CourseGoal";
 import CourseGoal from "./CourseGoal";
+import InfoBox from "./InfoBox";
+import { type ReactNode } from "react";
 
 interface CourseGoalListProps {
   goals: CourseGoalType[];
@@ -10,19 +12,39 @@ export default function CourseGoalList({
   goals,
   handleDeleteGoal,
 }: CourseGoalListProps) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goals yet. Start adding some!
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning">
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
   return (
-    <ul>
-      {goals.map((goal: CourseGoalType) => (
-        <li key={goal.id}>
-          <CourseGoal
-            handleDeleteGoal={handleDeleteGoal}
-            title={goal.title}
-            goalID={goal.id}
-          >
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal: CourseGoalType) => (
+          <li key={goal.id}>
+            <CourseGoal
+              handleDeleteGoal={handleDeleteGoal}
+              title={goal.title}
+              goalID={goal.id}
+            >
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
