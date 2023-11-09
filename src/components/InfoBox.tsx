80253984c9a0325407ab572fe454711fa6ff3b00
rbaruah <31023635+Rakesh6720@users.noreply.github.com>
2023-11-09 16:48:51 +0000

    Create InfoBox.tsx

---
+++ src/components/InfoBox.tsx
@@ -0,0 +1,15 @@
+import { type ReactNode } from "react";
+
+type InfoBoxProps = {
+  mode: "hint" | "warning";
+  children: ReactNode;
+};
+
+export default function InfoBox({ mode, children }: InfoBoxProps) {
+  return (
+    <aside>
+      {mode === "warning" ? <h2>Warning</h2> : null}
+      <p>{children}</p>
+    </aside>
+  );
+}
